fd241efd2bf315c077fe7f83cb888f3ba79dcffe
diegoberriogen <diego.berrio@genstudents.org>
2025-02-24 03:29:24 -0500

    Compl

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './src/**/*.{html,js}',  // Asegúrate de que este camino sea correcto según tu estructura de carpetas
+        './index.html',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
